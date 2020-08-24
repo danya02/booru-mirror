4e45a85aedb50ef086db781ea552f35f5632824d
Danya Generalov <danya@danya02.ru>
2020-08-24 09:43:43 +0100

    Misc improvements related to search.

--- server.py
+++ server.py
@@ -15,6 +15,16 @@ def index():
 
 ELEM_PER_PAGE = 50
 
+def get_post_counts_by_tag(all_tags):
+    tag_counts = dict()
+    tag_counts_cursor = db.execute_sql('''SELECT tag.name, COUNT(posttag.post_id)
+FROM tag INNER JOIN posttag ON posttag.tag_id = tag.id
+WHERE tag.name IN ''' + '(' + ', '.join([db.param] * len(all_tags)) + ''') 
+GROUP BY tag.id''', list(all_tags))
+    for tag_name, post_count in tag_counts_cursor.fetchall():
+        tag_counts[tag_name] = post_count
+    return tag_counts
+
 @app.route('/search')
 def search():
     query = request.args.get('q')
@@ -24,68 +34,138 @@ def search():
     def get_page(num):
         return url_for('search', q=query, p=num)    
 
+    # analyze query
     tags = query.split()
+    query_tags = []
+    ambiguous_tags = []
     tag_rows = []
     for i in tags:
-        tag_rows.append(Tag.get(Tag.name == i))
+        if i[0] == '-':
+            tag = i[1:]
+            invert = True
+        else:
+            tag = i
+            invert = False
 
-    posts_query = PostTag.select(PostTag.post_id).where(PostTag.tag == tag_rows[0])
-    for tagrow in tag_rows[1:]:
-        posts_query = posts_query.intersect(PostTag.select(PostTag.post_id).where(PostTag.tag == tagrow))
+        
+        query_tags.append(tag)
+        cnt = len(Tag.select().where(Tag.name == tag))
+        if cnt==0:
+            return render_template('search.html', query=query, cur_page=1, max_page=1, posts=[], get_post=lambda x: 'http://WTF/'+str(x), max=max, min=min, get_url_for_page=lambda x: 'http://WTF/page/'+str(x), tag_post_counts=dict(), tag_not_found=tag)
+        else:
+            exact_tag = Tag.get(SQL('BINARY t1.name = %s', [tag]))
+            if cnt>1:
+                tag_alternates = []
+                for opt in Tag.select().where(Tag.name==tag):
+                    print(opt, opt.name)
+                    tag_alternates.append(opt.name)
+                ambiguous_tags.append((tag, tag_alternates))
+            tag_rows.append((exact_tag, invert))
+
+    # build SQL query
+
+    pos_tags = []
+    neg_tags = []
+    for tag, invert in tag_rows:
+        if invert:
+            neg_tags.append(tag)
+        else:
+            pos_tags.append(tag)
+
+    if len(pos_tags)>0:
+        # first, create select by first positive statement
+        posts_query = PostTag.select(PostTag.post_id).where(PostTag.tag == pos_tags[0])
+        # then intersect it with other positive statements
+        for pos_tag in pos_tags[1:]:
+            posts_query = posts_query.intersect( PostTag.select(PostTag.post_id).where(PostTag.tag == pos_tag) )
+        # and except the negative statements
+        for neg_tag in neg_tags:
+            posts_query = posts_query.except_( PostTag.select(PostTag.post_id).where(PostTag.tag == neg_tag) ) 
+    else:
+        # it is not optimal to use except against full result set, so instead the query will be a NOT IN:
+        posts_query = PostTag.select(PostTag.post_id).where(PostTag.tag.not_in(neg_tags))
+    
 
 
-    max_page = math.ceil(posts_query.count()/ELEM_PER_PAGE)
+    cnt = posts_query.count()
+    max_page = math.ceil(cnt/ELEM_PER_PAGE)
     if page < 1 or page > max_page:
-        return redirect(get_page(1))
+        if cnt != 0:
+            return redirect(get_page(1))
+        else:
+            return render_template('search.html', query=query, cur_page=1, max_page=1, posts=[], get_post=None, max=max, min=min, get_url_for_page=None, tag_post_counts=dict(), ambiguous_tags=ambiguous_tags)
 
+    # get list of posts to display on this page
     posts_query = posts_query.paginate(page, ELEM_PER_PAGE)
 
     post_ids = [posttag.post_id for posttag in posts_query]
     post_tags = dict()
 
 
+    # for each post, get list of tags belonging to this post
     tag_sep = 'TAGseparator_tagSEPARATOR'
-    cursor = db.execute_sql('''SELECT post.id, GROUP_CONCAT(tag.name SEPARATOR %s)
+    post_tags_cursor = db.execute_sql('''SELECT post.id, GROUP_CONCAT(tag.name SEPARATOR %s)
 FROM post INNER JOIN posttag ON posttag.post_id = post.id
 INNER JOIN tag ON posttag.tag_id = tag.id
 WHERE post.id IN ''' + '('+ ', '.join([db.param] * len(post_ids)) + ')' + ''' GROUP BY post.id''', [tag_sep]+post_ids)
-    for row in cursor.fetchall():
+    for row in post_tags_cursor.fetchall():
         id, tags = row
-        post_tags[id]=tags.split(tag_sep)
+        post_tags[id] = tags.split(tag_sep)
 
+    # get list of post instances
     post_rows = list(Post.raw('''SELECT * FROM post
 WHERE post.id IN ''' + '('+ ', '.join([db.param] * len(post_ids)) + ')', *post_ids))
-
 
     post_rows.sort(key=lambda x: x.id)
     post_rows_dict = dict()
     for row in post_rows:
         post_rows_dict[row.id]=row
 
-    
+    # join post instances and tag lists
     post_list = []
     for id in post_ids:
         post_list.append( (post_rows_dict[id], post_tags[id]) )
 
 
+    # from tag lists, get tags 
+    all_tags = set()
+    for index, id in enumerate(post_tags):
+        for i in query_tags:
+            all_tags.discard(i)
+        if len(all_tags) > 50:
+            break
+        for tag in post_tags[id]:
+            all_tags.add(tag)
+            if len(all_tags) > 50:
+                break
+
+    # and get count of posts for each of the tags
+    tag_post_counts = get_post_counts_by_tag(all_tags)
+    
+
     def get_post(id):
         return url_for('view_post', id=id)
 
-    return render_template('search.html', query=query, cur_page=page,  max_page=max_page, posts=post_list, get_post=get_post, max=max, min=min, get_url_for_page=get_page)
+    return render_template('search.html', query=query, cur_page=page, max_page=max_page, posts=post_list, get_post=get_post, max=max, min=min, get_url_for_page=get_page, tag_post_counts=tag_post_counts, ambiguous_tags=ambiguous_tags)
 
 @app.route('/post/<int:id>')
 def view_post(id):
     post = Post.get_or_none(Post.id==id)
     if post is None:
         post = Post(id=id)
-        tags = 'DOES NOT EXIST'
         content = None
-        return render_template('post.html', post=post, tags=tags, unavail=UnavailablePost.get_or_none(UnavailablePost.id==id), content=Content.get_or_none(Content.post_id==id))
-    cursor = db.execute_sql('select GROUP_CONCAT(tag.name separator ", ") from post inner join posttag on posttag.post_id = post.id inner join tag on posttag.tag_id = tag.id where post.id = %s group by post.id', id)
+        return render_template('post.html', post=post, tags=[], tag_counts=dict(), unavail=UnavailablePost.get_or_none(UnavailablePost.id==id), content=Content.get_or_none(Content.post_id==id))
+    
+    tag_sep = 'TAGseparator_tagSEPARATOR'
+    cursor = db.execute_sql('''SELECT GROUP_CONCAT(tag.name SEPARATOR %s) 
+FROM post INNER JOIN posttag ON posttag.post_id = post.id INNER JOIN tag ON posttag.tag_id = tag.id 
+WHERE post.id = %s GROUP BY post.id''', [tag_sep, id])
     tags = ''
     for row in cursor.fetchall():
-        tags = row[0]
-    return render_template('post.html', post=post, tags=tags, unavail=UnavailablePost.get_or_none(UnavailablePost.id==id), content=Content.get_or_none(Content.post_id==id))
+        tags = row[0].split(tag_sep)
+    tag_counts = get_post_counts_by_tag(tags)
+    return render_template('post.html', post=post, tags=tags, tag_counts=tag_counts, unavail=UnavailablePost.get_or_none(UnavailablePost.id==id), content=Content.get_or_none(Content.post_id==id))
+
 
 @app.route('/post/<int:id>/update')
 def update_post(id):
@@ -143,6 +223,32 @@ def view_post_thumbnail(id):
         resp.mimetype = 'image/png'
         resp.cache_control.public = True
         resp.cache_control.immutable = True
+        resp.cache_control.max_age = 86400
+        return resp
+    def unknown_thumb(code):
+        # below is 150x150 "magenta ? on white field" PNG image
+        data = b'''iVBORw0KGgoAAAANSUhEUgAAAJYAAACWAQMAAAAGz+OhAAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9
+        kT1Iw0AcxV9bpUVaHSwo4pChOlkQFXHUKhShQqgVWnUwufQLmjQkKS6OgmvBwY/FqoOLs64OroIg
+        +AHi5Oik6CIl/i8ptIjx4Lgf7+497t4B/kaFqWbXOKBqlpFOJoRsblUIviKEAUTQi7DETH1OFFPw
+        HF/38PH1Ls6zvM/9OSJK3mSATyCeZbphEW8QT29aOud94igrSQrxOfGYQRckfuS67PIb56LDfp4Z
+        NTLpeeIosVDsYLmDWclQiaeIY4qqUb4/67LCeYuzWqmx1j35C8N5bWWZ6zSHkcQiliBCgIwayqjA
+        QpxWjRQTadpPePiHHL9ILplcZTByLKAKFZLjB/+D392ahckJNymcALpfbPtjBAjuAs26bX8f23bz
+        BAg8A1da219tADOfpNfbWuwI6NsGLq7bmrwHXO4Ag0+6ZEiOFKDpLxSA9zP6phzQfwv0rLm9tfZx
+        +gBkqKvUDXBwCIwWKXvd492hzt7+PdPq7wcZZ3KD4Jfq6QAAAAZQTFRF/wD/////nxgy4AAAAAlw
+        SFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+QIFw0SHa+VWIoAAAAZdEVYdENvbW1lbnQAQ3JlYXRl
+        ZCB3aXRoIEdJTVBXgQ4XAAABVUlEQVRIx+3VMa7DIAwAUEcZMnIEjsLR4GgcJUdgZIjob4ltjEPa
+        qOrwpTZLpKcktoMxcDtcGzyzBAfLAKDtTrD09niMHiSL1WxnlfBltG03kJbQvLCIZoUFNCMMCeZm
+        FAKmZpkMmiU2z7ayObbIZtkCm2FjqhWfWmk2k23NpmeW94gBC6mW9gqythlDebQVkwjKLJZIFrH4
+        db+zefyuNPq1ZAF/urKZlsWiUVeU12bO7NascOe9Z+5Ne1Q6sLZxPmnlxJyy7aLlE/PK0sBW0Wt0
+        xYEF0buiNG1F7gWR8qIsDyx1e7ClYpWFbk+3+eR72/oZwWEnZamfORx2URbUvKKwrrei559MRVim
+        sN0cVzMWF8goi9zBzQKFFXac7dVmZeV4LtSUzcDs4JxxA/PK0uA8SrgY0lZO76Ut2uLYzEWz2sLn
+        zV06z382NBis28++z/5rn/4BxaaJMuHWJqYAAAAASUVORK5CYII=
+        '''
+        data = base64.b64decode(data)
+        resp = make_response(data, code)
+        resp.mimetype = 'image/png'
+        resp.cache_control.public = True
+        resp.cache_control.immutable = True
         resp.cache_control.max_age = 60  # short because issue causing absence of thumb may be resolved soon
         return resp
     try:
@@ -152,7 +258,7 @@ def view_post_thumbnail(id):
 
     data, type = post.get_thumb()
     if data is None:
-        return no_thumb(200)
+        return unknown_thumb(200)
 
     resp = make_response(data)
     resp.mimetype = type
